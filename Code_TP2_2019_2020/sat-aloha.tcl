010a7e0f928235df21e824fd6914221d8ef80cc9
NextFire <git@yuru.moe>
2022-05-11 09:51:18 +0200

    tp2 suite
    Co-authored-by: xcnwf <xcnwf@users.noreply.github.com>

--- Code_TP2_2019_2020/sat-aloha.tcl
+++ Code_TP2_2019_2020/sat-aloha.tcl
@@ -1,26 +1,26 @@
 # $Header: /cvsroot/nsnam/ns-2/tcl/ex/sat-aloha.tcl,v 1.5 2001/11/06 06:20:11 tomh Exp $
-# 
-# Simple script with a geostationary bent-pipe (repeater) satellite and 
+#
+# Simple script with a geostationary bent-pipe (repeater) satellite and
 # one hundred terminals using unslotted (pure) Aloha random access.  The
 # traffic sources consist of exponential on-off traffic generators unless
-# indicated below.  With the traffic rates configured below, approximately 
+# indicated below.  With the traffic rates configured below, approximately
 # one tenth of the packets collide and must be retransmitted.
-# 
+#
 # Tests:
 # 1.  basic:  Mac operates in stop-and-wait mode (one outstanding packet
 #             at a time).  Collisions and drops are not traced.  This test
 #             should provide similar results to the satellite test suite.
 # 2.  basic_tracing:  Same as "basic", but drops ("d") and collisions ("c")
-#             are instead explicitly traced.  Again, similar to test suite   
-# 3.  poisson:  Packets arrive according to Poisson process.  Each source 
-#             still operates in stop-and-wait mode, and collisions and 
+#             are instead explicitly traced.  Again, similar to test suite
+# 3.  poisson:  Packets arrive according to Poisson process.  Each source
+#             still operates in stop-and-wait mode, and collisions and
 #             drops are traced.  rtx_limit_ == 0 (no persistence).
-#	      This can be used to try to approximate theoretical 
+#	      This can be used to try to approximate theoretical
 #	      unslotted Aloha results, if the number of terminals is large
 #             compared to the arrival rate (so that no packets are queued)
 # 4.  [FOR FUTURE WORK].  (larger than one packet rxmit buffer, different
 #             traffic generator?)
-# 
+#
 
 if { $argc != 1 } {
         puts stderr {usage: ns sat-aloha.tcl [basic basic_tracing poisson]}
@@ -34,9 +34,9 @@ global ns
 set ns [new Simulator]
 
 # Global configuration parameters for Aloha (also settable in ns-sat.tcl)
-Mac/Sat/UnslottedAloha set mean_backoff_ 1s ; # mean exponential backoff time(s)
-Mac/Sat/UnslottedAloha set rtx_limit_ 3; # max number of retrans. attempted 
-Mac/Sat/UnslottedAloha set send_timeout_ 270ms; # resend if send times out
+Mac/Sat/UnslottedAloha set mean_backoff_ 1s ; # mean exponential backoff time(s) TODO:
+Mac/Sat/UnslottedAloha set rtx_limit_ 3; # max number of retrans. attempted  TODO:
+Mac/Sat/UnslottedAloha set send_timeout_ 270ms; # resend if send times out TODO: ici ?
 
 if { $test_ == "basic"} {
 	Mac/Sat set trace_collisions_ false
@@ -50,17 +50,17 @@ set opt(bw_down)	2Mb
 set opt(phy)            Phy/Sat
 set opt(mac)            Mac/Sat/UnslottedAloha
 set opt(ifq)            Queue/DropTail
-set opt(qlim)		50		
+set opt(qlim)		50		; #TODO:
 set opt(ll)             LL/Sat
 set opt(wiredRouting)   OFF
 
-# XXX This tracing enabling must precede link and node creation 
+# XXX This tracing enabling must precede link and node creation
 set outfile [open out.tr w]
 $ns trace-all $outfile
 
 # Set up satellite and terrestrial nodes
 
-# GEO satellite at 0 degrees longitude 
+# GEO satellite at 0 degrees longitude
 $ns node-config -satNodeType geo-repeater \
 		-llType $opt(ll) \
 		-ifqType $opt(ifq) \
@@ -98,7 +98,7 @@ for {set a 1} {$a <= $num_nodes} {incr a} {
 	if {$test_ == "poisson"} {
 		$exp($a) set rate_ 10000Mb
 		$exp($a) set burst_time_ 0
-		$exp($a) set idle_time_ 5   
+		$exp($a) set idle_time_ 0.12
 		#0.17
 	}
 
@@ -118,7 +118,7 @@ $satrouteobject_ compute_routes
 $ns at 100.0 "finish"
 
 proc finish {} {
-	global ns outfile 
+	global ns outfile
 	$ns flush-trace
 	close $outfile
 
